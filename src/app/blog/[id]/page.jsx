461dc0499169da31c93467385fbc7c94d4382f2e
Sadia492 <sadiaafrin2792>
2025-01-23 20:47:59 +0600

    home page blogs fetched

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function BlogDetails({ params }) {
+  const { id } = params;
+  console.log(id);
+
+  return <div>details</div>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return <div>Home</div>;
+export default async function Home() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-4">
+        {data.map((blog) => (
+          <Link href={`/blog/${blog.id}`} key={blog.id}>
+            <div className="card bg-base-100 shadow-xl h-full">
+              <div className="card-body">
+                <h2 className="card-title">{blog.title}</h2>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
